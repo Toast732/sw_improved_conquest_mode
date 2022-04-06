c5a1a1d971a218f90c4e071fe870f2ba0086b7ff
Toast732 <55813762+Toast732@users.noreply.github.com>
2022-04-05 21:09:43 -0400

    (0.3.0.25) Changed github file structure | now includes the land pathfinding lua script in the github

--- Addon/script.lua
+++ Addon/script.lua
@@ -11,7 +11,7 @@ local s = server
 local m = matrix
 local sm = spawnModifiers
 
-local IMPROVED_CONQUEST_VERSION = "(0.3.0.24)"
+local IMPROVED_CONQUEST_VERSION = "(0.3.0.25)"
 
 -- valid values:
 -- "TRUE" if this version will be able to run perfectly fine on old worlds 
