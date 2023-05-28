0462462e90e602632edea2f66866216a5a1b9e93
Toast732 <55813762+Toast732@users.noreply.github.com>
2023-05-28 15:39:26 -0400

    Updated Format of Copyright Notice

--- Addon/_build/out/release/main.lua
+++ Addon/_build/out/release/main.lua
@@ -1,17 +1,38 @@
+--[[
+	
+Copyright 2022 Liam Matthews
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+]]
  
---? Copyright 2022 Liam Matthews
+--[[
+	
+Copyright 2022 Liam Matthews
 
---? Licensed under the Apache License, Version 2.0 (the "License");
---? you may not use this file except in compliance with the License.
---? You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
---?		http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
---? Unless required by applicable law or agreed to in writing, software
---? distributed under the License is distributed on an "AS IS" BASIS,
---? WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
---? See the License for the specific language governing permissions and
---? limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+]]
 
 --! (If gotten from Steam Workshop) LICENSE is in vehicle_0.xml
 --! (If gotten from anywhere else) LICENSE is in LICENSE and vehicle_0.xml
